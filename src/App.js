47a6bd4d244c862d0876e08ebe4594d7d91340a8
Tim <timlai@live.com>
2022-02-22 21:40:42 -0500

    mmm

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           >
           </a>
           <h3>
-            CloudAcademy ❤ DevOps 2019 - version 2.3a
+            CloudAcademy ❤ DevOps 2019 - version 2.4
           </h3>
         </header>
       </div>
